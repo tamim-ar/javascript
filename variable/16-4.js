2f4b1db96bf0d0dc90323043c3b3670d6a4744d2
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2024-08-28 17:26:31 +0600

    first

---
+++ variable/16-4.js
@@ -0,0 +1,25 @@
+var num = 1;
+var num2 = 2;
+
+console.log(num + num2);
+var sum = num + num2;
+var difference = num - num2;
+var product = num * num2;
+var quotient = num / num2;
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+
+var realmadrid = true;
+var barcelona = false;
+
+console.log(realmadrid);    
+console.log(barcelona);
+
+// true false reserved word for javascript
+
+console.log(typeof realmadrid);
+console.log(typeof barcelona);
+console.log(typeof sum);
---
+++ variable/16-5.js
@@ -0,0 +1,8 @@
+var isbestclub = true;
+console.log(isbestclub);
+console.log(typeof isbestclub);
+
+var myname = "John";
+var my_name = "Doe"; // snake_case
+var myName = "Doe"; // camelCase
+var MyName = "Doe"; // PascalCase
