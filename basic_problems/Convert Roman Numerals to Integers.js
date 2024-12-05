2e704a54e3735088ef800d551e643889f092e1a4
Tamim Ahasan Rijon <tamim15-3289@diu.edu.bd>
2024-12-05 23:38:32 +0600

    Convert Roman Numerals to Integers

---
+++ basic_problems/Convert Roman Numerals to Integers.js
@@ -0,0 +1,19 @@
+function romanToInt(roman) {
+    const romanMap = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+    let total = 0;
+
+    for (let i = 0; i < roman.length; i++) {
+        const current = romanMap[roman[i]];
+        const next = romanMap[roman[i + 1]];
+
+        if (current < next) {
+            total -= current;
+        } else {
+            total += current;
+        }
+    }
+    return total;
+}
+
+console.log(romanToInt("XIV")); // 14
+console.log(romanToInt("MCMXCIV")); // 1994
