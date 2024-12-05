f071fa375b915160fff9014cc1eff45e13eb5f00
Tamim Ahasan Rijon <tamim15-3289@diu.edu.bd>
2024-12-05 23:36:44 +0600

    Group Array Items by Key

---
+++ basic_problems/Group Array Items by Key.js
@@ -0,0 +1,18 @@
+function groupBy(arr, key) {
+    return arr.reduce((acc, obj) => {
+        const group = obj[key];
+        acc[group] = acc[group] || [];
+        acc[group].push(obj);
+        return acc;
+    }, {});
+}
+
+const items = [
+    { type: "fruit", name: "apple" },
+    { type: "vegetable", name: "carrot" },
+    { type: "fruit", name: "banana" }
+];
+
+console.log(groupBy(items, "type"));
+// { fruit: [{ type: 'fruit', name: 'apple' }, { type: 'fruit', name: 'banana' }],
+//   vegetable: [{ type: 'vegetable', name: 'carrot' }] }
