c5beedd3bf64f6d871c1bca2e16948afdcf8bf70
Tamim Ahasan Rijon <tamim15-3289@diu.edu.bd>
2024-12-05 23:22:13 +0600

    Capitalize Each Word

---
+++ basic_problems/Capitalize Each Word.js
@@ -0,0 +1,5 @@
+function capitalizeWords(str) {
+    return str.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
+}
+
+console.log(capitalizeWords("hello world from javascript")); // "Hello World From Javascript"
