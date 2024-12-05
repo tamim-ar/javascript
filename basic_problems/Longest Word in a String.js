e077f3accd21af1b5a98edbe980f1f9432da7945
Tamim Ahasan Rijon <tamim15-3289@diu.edu.bd>
2024-12-05 23:39:02 +0600

    Longest Word in a String

---
+++ basic_problems/Longest Word in a String.js
@@ -0,0 +1,5 @@
+function findLongestWord(str) {
+    return str.split(' ').reduce((longest, word) => word.length > longest.length ? word : longest, '');
+}
+
+console.log(findLongestWord("JavaScript is awesome")); // "JavaScript"
