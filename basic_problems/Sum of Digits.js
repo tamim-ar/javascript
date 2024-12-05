99f690b808300759e7ac9d1095cec86b3fc4792f
Tamim Ahasan Rijon <tamim15-3289@diu.edu.bd>
2024-12-05 23:38:12 +0600

    Sum of Digits

---
+++ basic_problems/Sum of Digits.js
@@ -0,0 +1,9 @@
+function sumOfDigits(num) {
+    return num
+        .toString()
+        .split('')
+        .map(Number)
+        .reduce((sum, digit) => sum + digit, 0);
+}
+
+console.log(sumOfDigits(12345)); // 15
