415b2b436ef39e26cf850ebb99774053b0a1c1a6
Tamim Ahasan Rijon <77635914+tamim-ar@users.noreply.github.com>
2025-01-18 21:42:30 +0600

    Update Capitalize Each Word.js

--- basic_problems/Capitalize Each Word.js
+++ basic_problems/Capitalize Each Word.js
@@ -2,4 +2,4 @@ function capitalizeWords(str) {
     return str.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
 }
 
-console.log(capitalizeWords("hello world from javascript")); // "Hello World From Javascript"
+console.log(capitalizeWords("hello world from javascript"));
