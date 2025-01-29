fbbe7392e84191669d8beff67b975bca7b2135cf
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-01-29 23:59:31 +0600

    Fix formatting: remove unnecessary comments and add missing newlines in push-pop.js

--- excercise/array/push-pop.js
+++ excercise/array/push-pop.js
@@ -1,14 +1,13 @@
 var numbers = [1, 2, 3, 4, 5];
 
-console.log(numbers); // 6
+console.log(numbers); 
 
 numbers.push(6)
 
-console.log(numbers); // 6
+console.log(numbers); 
 
 numbers.pop();
 
 numbers.pop();
 
-
-console.log(numbers); // 6
+console.log(numbers); 
