13d23f3fdb50a5d2f106c4cdc31285367c11b27d
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-02-09 00:19:03 +0600

    Add object example with properties and console logs in object.js

---
+++ excercise/string/object.js
@@ -0,0 +1,20 @@
+const man = {
+    name: 'tamim',
+    gender: 'male',
+    age: 25,
+    married: false,
+    'gf name': false
+}
+
+console.log(man)
+console.log(man.age)
+console.log(man['married'])
+console.log(man['gf name'])
+
+let a=2;
+
+console.log(a);
+
+a = 3;
+
+console.log(a)
