95af4be03d789fcba61efe0b34be064fb917469f
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-02-08 23:50:00 +0600

    reverse

--- excercise/string/reverse.js
+++ excercise/string/reverse.js
@@ -1,5 +1,12 @@
 const name = "Tamim Ahasan Rijon";
 
+// for (const letter of name){
+//     console.log(letter);
+// }
+
+let reverse = '';
 for (const letter of name){
-    console.log(letter);
+    reverse = letter + reverse;
 }
+
+console.log(reverse);
