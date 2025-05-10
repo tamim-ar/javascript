72e080da2bcc5eb0ffd21cb4bb007aeb5d422c91
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-05-10 22:23:02 +0600

    Remove comment from console log statement in removeDuplicates function

--- problems/Remove Duplicates from an Array.js
+++ problems/Remove Duplicates from an Array.js
@@ -2,4 +2,4 @@ function removeDuplicates(arr) {
     return [...new Set(arr)];
 }
 
-console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // [1, 2, 3, 4, 5]
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); 
