bb82b57016e623135bd471d359b319bf367a26ef
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-05-10 22:25:07 +0600

    Remove commented output from console log statement in Filter Even Numbers.js

--- problems/Filter Even Numbers.js
+++ problems/Filter Even Numbers.js
@@ -2,4 +2,4 @@ function filterEvenNumbers(arr) {
     return arr.filter(num => num % 2 === 0);
 }
 
-console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6])); // [2, 4, 6]
+console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6])); 
