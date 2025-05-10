0c6dfcb0d88a0f18b28f5d0a72d00c88fe326f96
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-05-10 22:23:47 +0600

    Remove comment from console log statement in randomBetween function

--- problems/Generate a Random Number.js
+++ problems/Generate a Random Number.js
@@ -2,4 +2,4 @@ function randomBetween(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log(randomBetween(1, 100)); // Random number between 1 and 100
+console.log(randomBetween(1, 100)); 
