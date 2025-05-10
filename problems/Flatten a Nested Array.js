fcb9f4392c17b1d7c677a08b5f6bf00f096016e7
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-05-10 22:24:12 +0600

    Remove commented output from console log statement in flattenArray function

--- problems/Flatten a Nested Array.js
+++ problems/Flatten a Nested Array.js
@@ -2,4 +2,4 @@ function flattenArray(arr) {
     return arr.flat(Infinity);
 }
 
-console.log(flattenArray([1, [2, [3, [4, 5]]]])); // [1, 2, 3, 4, 5]
+console.log(flattenArray([1, [2, [3, [4, 5]]]])); 
